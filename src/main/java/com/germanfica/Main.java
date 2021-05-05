a9a92667821cb8ad182ff5d335727a2672327203
German Fica <germafica@gmail.com>
2021-05-04 23:40:06 -0300

    Logback configuration

--- src/main/java/com/germanfica/Main.java
+++ src/main/java/com/germanfica/Main.java
@@ -1,11 +1,18 @@
 package com.germanfica;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Scanner;
 
 public class Main {
+    private final static Logger log = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello manven!");
-        Scanner scanner = new Scanner(System.in);
-        scanner.nextLine();
+        //System.out.println("Hello manven!");
+        //Scanner scanner = new Scanner(System.in);
+        //scanner.nextLine();
+        log.info("Hello Info");
+        log.debug("Hello Debug");
     }
 }
